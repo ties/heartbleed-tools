34952f85a55fbd03bb200fe705037310c93f5b3f
Ties de Kock <ties@tiesdekock.nl>
2014-05-07 11:08:13 +0200

    large file support and exact division
    On my machine (OS X 10.9.2/Python 3.4.0 from Homebrew) the script did
    not work with a large (> 4GB dump).
    
    The scripts were modified to use mmap instead of reading the whole file
    into memory.
    
    Another small patch was needed to fix a division. n/p =  q returned even
    results, which was odd. Using gmpy for this division fixes this on my
    machine.

--- keydump.py
+++ keydump.py
@@ -6,7 +6,7 @@ Environment: Python 3
 """  
 from __future__ import print_function
 import sys, struct, subprocess, binascii, re, base64
-import gmpy 
+import gmpy, mmap 
 from pyasn1.codec.der import encoder
 from pyasn1.type.univ import *
 
@@ -33,35 +33,39 @@ def hexdump(data, offset, size):
     
 def main(cert_path, data_path, offset):
   mod = get_modulus(cert_path)
-  mod = int(mod, 16)
+  mod = gmpy.mpz(mod, 16)
   key_size = int(mod.bit_length() / 16)
   offset = int(offset, 16)
   print('Prime should be at offset: %x'%offset)
   print('Key size: %d\n'%key_size)
   with open(data_path, 'rb') as f:
-    data = f.read()
-  print('Hexdump of surrounding data:')
-  hexdump(data, max(0, offset - 1024), 2048)
-  p = long(data, offset, key_size)
-  if gmpy.is_prime(p) and p != mod and mod % p == 0:
-    print('Prime factor found: %s\n'%p)
-    hexbytes = binascii.hexlify(data[offset:offset+key_size]).decode('ascii').upper()
-    hexbytes = re.sub(r'(..)', r'\x\1', hexbytes)
-    print('Prime in greppable ascii: %s\n'%hexbytes)
-    p = gmpy.mpz(p)
-    e = gmpy.mpz(65537)
-    q = gmpy.mpz(mod / p)
-    phi = (p-1) * (q-1)
-    d = gmpy.invert(e, phi)
-    dp = d % (p - 1)
-    dq = d % (q - 1)
-    qinv = gmpy.invert(q, p)
-    seq = Sequence()
-    for x in [0, mod, e, d, p, q, dp, dq, qinv]:
-      seq.setComponentByPosition (len (seq), Integer (x))
-    print("\n\n-----BEGIN RSA PRIVATE KEY-----\n%s-----END RSA PRIVATE KEY-----\n\n"%base64.encodestring(encoder.encode(seq)).decode('ascii'))
-  else:
-    print('Prime factor not found')
+    mm_data = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+
+    print('Hexdump of surrounding data:')
+    hexdump(mm_data, max(0, offset - 1024), 2048)
+    p = gmpy.mpz(long(mm_data, offset, key_size))
+    if gmpy.is_prime(p) and p != mod and mod % p == 0:
+      print('Prime factor found: %s\n'%p)
+      hexbytes = binascii.hexlify(mm_data[offset:offset+key_size]).decode('ascii').upper()
+      hexbytes = re.sub(r'(..)', r'\x\1', hexbytes)
+      print('Prime in greppable ascii: %s\n'%hexbytes)
+      p = gmpy.mpz(p)
+      e = gmpy.mpz(65537)
+      q = gmpy.divexact(mod, p)
+      
+      assert(gmpy.is_prime(q))
+
+      phi = (p-1) * (q-1)
+      d = gmpy.invert(e, phi)
+      dp = d % (p - 1)
+      dq = d % (q - 1)
+      qinv = gmpy.invert(q, p)
+      seq = Sequence()
+      for x in [0, mod, e, d, p, q, dp, dq, qinv]:
+        seq.setComponentByPosition (len (seq), Integer (x))
+      print("\n\n-----BEGIN RSA PRIVATE KEY-----\n%s-----END RSA PRIVATE KEY-----\n\n"%base64.encodestring(encoder.encode(seq)).decode('ascii'))
+    else:
+      print('Prime factor not found')
     
 if __name__ == '__main__':
   sys.exit(main(*sys.argv[1:]))
--- keyscan.py
+++ keyscan.py
@@ -4,7 +4,7 @@ Traverse memory dump, looking for prime factors.
 Author: Einar Otto Stangvik / @einaros / https://hacking.ventures
 Environment: Python 3
 """  
-import sys, subprocess
+import sys, subprocess, mmap, os
 
 def get_modulus(cert_path):
   return subprocess.Popen(
@@ -20,26 +20,27 @@ def long(data, offset, size):
   return n
     
 def main(cert_path, data_path):
+  assert(os.path.isfile(cert_path) and os.path.isfile(data_path))
   mod = get_modulus(cert_path)
   mod = int(mod, 16)
   key_size = int(mod.bit_length() / 16)
   print('Key size: %d'%key_size)
   with open(data_path, 'rb') as f:
-    data = f.read()
-  print('Data length: %d'%len(data))
-  length = len(data) - key_size
-  for i in range(length):
-    if i % 100000 == 0:
-      sys.stdout.write(chr(27) + '[%dG'%(1) + chr(27) + '[0K')
-      sys.stdout.write('Progress: %d%%'%(100.0*i/length))
-      sys.stdout.flush()
-    if data[i] % 2 == 0:
-      continue
-    p = long(data, i, key_size)
-    if p != 0 and p != 1 and p != mod and mod % p == 0:
-      sys.stdout.write(chr(27) + '[%dG'%(1) + chr(27) + '[0K')
-      print('%s Offset 0x%x: p = %s'%(data_path, i, p))
-  sys.stdout.write(chr(27) + '[%dG'%(1) + chr(27) + '[0K')
+    mm_data = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+    print('Data length: %d'%mm_data.size())
+    length = mm_data.size() - key_size
+    for i in range(length):
+      if i % 100000 == 0:
+        sys.stdout.write(chr(27) + '[%dG'%(1) + chr(27) + '[0K')
+        sys.stdout.write('Progress: %d%%'%(100.0*i/length))
+        sys.stdout.flush()
+      if mm_data[i] % 2 == 0:
+        continue
+      p = long(mm_data, i, key_size)
+      if p != 0 and p != 1 and p != mod and mod % p == 0:
+        sys.stdout.write(chr(27) + '[%dG'%(1) + chr(27) + '[0K')
+        print('%s Offset 0x%x: p = %s'%(data_path, i, p))
+    sys.stdout.write(chr(27) + '[%dG'%(1) + chr(27) + '[0K')
     
 if __name__ == '__main__':
   sys.exit(main(*sys.argv[1:]))
